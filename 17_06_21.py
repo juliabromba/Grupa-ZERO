05d72b3a8c7ad929d93a3b8be18f3f5edfe9cf35
LukaszKniter <80036508+LukaszKniter@users.noreply.github.com>
2021-06-18 00:17:03 +0200

    Update 17_06_21.py

--- 17_06_21.py
+++ 17_06_21.py
@@ -3,6 +3,9 @@ import random
 import csv
 import os, sys
 from random import shuffle
+from pathlib import Path
+
+win = visual.Window([1520, 880], units="pix", color="white", fullscr=False)
 
 happypath = "C:/Users/Laptop/Desktop/Informatyka projekt/happy"
 happy = list(os.listdir(happypath))
@@ -48,7 +51,23 @@ def happy_niezgodny(angry, happy):
     return happy2
 
 
-print(happy_zgodny(happy))
-print(angry_zgodny(angry))
-print(angry_niezgodny(angry, happy))
-print(happy_niezgodny(angry, happy))
+a = happy_zgodny(happy)
+b = angry_zgodny(angry)
+c = angry_niezgodny(angry,happy)
+d = happy_niezgodny(angry, happy)
+
+trial1_center = os.path.abspath(a[1])
+trial1_left = os.path.abspath(a[0])
+trial1_right = os.path.abspath(a[2])
+
+srodek = visual.ImageStim(win, image=trial1_center, pos=(0.0, 0.0), size=[300, 377], colorSpace='rgb')
+lewy = visual.ImageStim(win, image=trial1_left, pos=(-310.0, 0.0), size=[300, 377], colorSpace='rgb')
+prawy = visual.ImageStim(win, image=trial1_right, pos=(310.0, 0.0), size=[300, 377], colorSpace='rgb')
+srodek.draw()
+prawy.draw()
+lewy.draw()
+win.flip()
+core.wait(6)
+
+win.flip()
+win.close()
